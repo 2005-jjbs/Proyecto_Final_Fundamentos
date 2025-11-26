742afeef301e6fd69e7b6bc4ff19f815ce2dc70f
Fer :) <mafevelandia0918@gmail.com>
2025-11-26 03:53:56 +0000

    Nuevo Catálogo

--- redsocial/src/main/java/com/ingesoft/redsocial/ui/CatalogoView.java
+++ redsocial/src/main/java/com/ingesoft/redsocial/ui/CatalogoView.java
@@ -1,30 +1,17 @@
-package com.ingesoft.redsocial.ui;
-
-import java.util.List;
-
-import com.ingesoft.redsocial.modelo.Producto;
-import com.ingesoft.redsocial.servicios.ProductoService;
-import com.ingesoft.redsocial.ui.componentes.NavegacionComponent;
-import com.vaadin.flow.component.grid.Grid;
-import com.vaadin.flow.component.html.Main;
-import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.router.PageTitle;
-import com.vaadin.flow.router.Route;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
 @Route("catalogo")
 @PageTitle("Catálogo de Productos")
 public class CatalogoView extends Main {
 
+    private final ProductoService productoService;
+    private final NavegacionComponent navegacion;
+
+    private final Grid<Producto> grid;
+
     @Autowired
-    ProductoService productoService;
+    public CatalogoView(ProductoService productoService,
+                        NavegacionComponent navegacion) {
 
-    NavegacionComponent navegacion;
-
-    Grid<Producto> grid;
-
-    public CatalogoView(NavegacionComponent navegacion) {
+        this.productoService = productoService;
         this.navegacion = navegacion;
 
         setSizeFull();
@@ -43,16 +30,13 @@ public class CatalogoView extends Main {
         grid.addColumn(Producto::getEstado).setHeader("Estado");
 
         layout.add(grid);
-
         add(layout);
 
         actualizarGrid();
     }
 
     private void actualizarGrid() {
-        List<Producto> productos = productoService.listarTodos();
-        grid.setItems(productos);
+        grid.setItems(productoService.listarTodos());
     }
 
 }
-
